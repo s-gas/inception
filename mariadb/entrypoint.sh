8d925136619374680143261e2525f0f401dd1032
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2026-03-24 17:53:19 +0100

    Fix bash script for mariadb

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+set -e
 
-mkdir -p /run/mysqld
-chown mysql:mysql /run/mysqld
+MARKER_FILE="/var/lib/mysql/.db_initialized"
 
-mysqld --user=mysql &
-sleep 5
+if [ ! -f "$MARKER_FILE" ]; then
+    mariadb-install-db --user=mysql --ldata=/var/lib/mysql
 
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';"
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${DATABASE_USER}'@'%';"
-mariadb -e "FLUSH PRIVILEGES;"
+    mysqld_safe --datadir=/var/lib/mysql &
+    pid="$!"
 
-mysqladmin shutdown
+    while [ ! -S /var/run/mysqld/mysqld.sock ]; do
+        sleep 1
+    done
 
-exec mysqld --user=mysql
+    mariadb -u root -S /var/run/mysqld/mysqld.sock -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
+    mariadb -u root -S /var/run/mysqld/mysqld.sock -e "CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+    mariadb -u root -S /var/run/mysqld/mysqld.sock -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%';"
+    mariadb -u root -S /var/run/mysqld/mysqld.sock -e "FLUSH PRIVILEGES;"
+
+    touch "$MARKER_FILE"
+    echo "[MariaDB] Initialization complete"
+
+    mysqladmin -u root -S /var/run/mysqld/mysqld.sock shutdown
+    wait "$pid"
+fi
+
+exec mysqld_safe --datadir=/var/lib/mysql --bind-address=0.0.0.0
--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,6 +5,6 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -out /etc/ssl/certs/nginx.crt \
     -subj "/CN=${DOMAIN_NAME}"
 
-RUN sed -i "s|SERVER_NAME|${DOMAIN_NAME}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|SERVER_NAME|${DOMAIN_NAME}|g" /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
