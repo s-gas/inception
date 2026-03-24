fc11f986187ccc108bdebbaaa904ccd56272e2bc
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2026-03-24 14:38:00 +0100

    Store environment variables in .env file

--- mariadb/mariadb_init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-
-CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'wordpresspass';
-
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'%';
-
-FLUSH PRIVILEGES;
